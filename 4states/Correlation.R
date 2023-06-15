e4a49aa8d853b8b5d42fca6fda96ea3191b52b1f
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2023-06-15 17:24:01 +0530

    4states cellline correlation fixed

--- 4states/Correlation.R
+++ 4states/Correlation.R
@@ -4,7 +4,7 @@ library(data.table)
 library(readxl)
 
 corr_df = function(GSE, GSM){
-    df = mcreadRDS(paste0("./Data_generated/",GSE,"/",GSM,"_counts.rds"), mc.cores=4)
+    df = mcreadRDS(paste0("./Data_generated/",GSE,"/",GSM,"_imputed.rds"), mc.cores=4)
     gbmgenes = read_excel("./Signatures/1-s2.0-S0092867419306877-mmc2.xlsx",skip = 4)
     gbmgenes = gbmgenes[,0:6]
     # add values in all columns of gbmgenes to a list
@@ -13,6 +13,8 @@ corr_df = function(GSE, GSM){
     genes = genes[! is.na(genes) & (genes %in% rownames(df))]
     # Select the rows in df based on values in gbmgenes
     df = df[genes,]
+    # Remove rows with 0 sd
+    df = df[!apply(df,1,sd)==0,]
     corrdf = data.frame(cor(t(df),method="spearman"))
     fwrite(corrdf, paste0("./Output/",GSE,"/",GSM,"_correlation.tsv"), sep='\t', col.names=TRUE, row.names=TRUE)
 }
