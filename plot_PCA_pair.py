40247e9159635d4c05f58e9b4ba0974321984bab
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2023-07-19 11:21:48 +0530

    More spaghett with more combinations

--- plot_PCA_pair.py
+++ plot_PCA_pair.py
@@ -5,9 +5,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn as sns
+import itertools as it
 plt.rcParams["svg.hashsalt"]=''
 
-def loada_barplot(GSE, GSM, suff):
+def loada_barplot(GSE, GSM, gs):
+    suff = '-'.join(gs)
     print(GSM,suff)
     # Load the loadings matrix
     load_df = pd.read_csv('./Output/'+GSE+'/PCA/'+GSM+'_loadings_'+suff+'.tsv',sep='\t',index_col=0)
@@ -17,7 +19,6 @@ def loada_barplot(GSE, GSM, suff):
     # Read the GBM signature
     gbmgenes = pd.read_csv("./Signatures/GBM_signatures.csv")
     # Select only the columns that start with suff
-    gs = suff.split('-')
     gbmgenes = gbmgenes.loc[:,gs]
     # select only the genes in the signature and merge with load_df
     gbmgenes = gbmgenes.melt(var_name='Signature').dropna()
@@ -31,25 +32,43 @@ def loada_barplot(GSE, GSM, suff):
     plt.close()
     plt.clf()
 
-def expvar_plot(GSE,GSM,gs,suff):
+def expvar_plot(GSE,GSM,suff):
+    gbmgenes = pd.read_csv("./Signatures/GBM_signatures.csv")
+    # Select only the columns that start with suff
+    gbmgenes = gbmgenes.filter(like=suff)
+    # Make combinations of genes
+    gs = list(it.combinations(gbmgenes.columns,2))
     gs.append(suff)
     li = []
     # Read the variance explained
     for g in gs:
+        if isinstance(g, tuple):
+            g = '-'.join(g)
         df = pd.read_csv('./Output/'+GSE+'/PCA/'+GSM+'_expvar_'+g+'.tsv',sep='\t',header=None)
         df = df*100
         # Concatenate the variance explained
-        li.append([df.iloc[0,0]])
+        li.append(df.iloc[0,0])
     # Make a dataframe of the variance explained with index the gs
-    df = pd.DataFrame(li,index=gs, columns=['Variance_explained'])
-    sns.barplot(data=df, x=df.index, y='Variance_explained')
+    plt.bar(range(len(gs)),li)
+    plt.xticks(range(len(gs)), gs, rotation=45)
+    plt.ylabel('Variance explained')
+    plt.tight_layout()
     plt.savefig('./figures/'+GSE+'/PCA/'+GSM+'_expvar_'+suff+'.svg')
     plt.close()
     plt.clf()
+
+def loada_pair_barplot(GSE, GSM, suff):
+    gbmgenes = pd.read_csv("./Signatures/GBM_signatures.csv")
+    # Select only the columns that start with suff
+    gbmgenes = gbmgenes.filter(like=suff)
+    # Make combinations of genes
+    gs = list(it.combinations(gbmgenes.columns,2))
+    for g in gs:
+        loada_barplot(GSE, GSM, g)
     
 # GSE ID
-# GSE = "GSE168004"
-GSE = "GSE131928"
+GSE = "GSE168004"
+# GSE = "GSE131928"
 # GSE = "GSE182109"
 # GSE = "CCLE"
 # GSE = "TCGA"
@@ -61,11 +80,7 @@ files = glob.glob('Output/'+GSE+'/PCA/*_loadings_Nef.tsv')
 GSMs = [os.path.basename(x).replace(f'_loadings_Nef.tsv','') for x in files]
 # Iterate over GSM samples
 for GSM in GSMs:
-    gs = ['NefNPC-VerMES','NefOPC-NefAC']
-    for suff in gs:
-        loada_barplot(GSE,GSM,suff)
-    expvar_plot(GSE,GSM, gs,'Nef')
-    gs = ['VerPN-VerMES','VerCL-VerNL']
-    for suff in gs:
-        loada_barplot(GSE,GSM,suff)
-    expvar_plot(GSE,GSM,gs,'Ver')
+    loada_pair_barplot(GSE,GSM,'Nef')
+    expvar_plot(GSE,GSM,'Nef')
+    loada_pair_barplot(GSE,GSM,'Ver')
+    expvar_plot(GSE,GSM,'Ver')
