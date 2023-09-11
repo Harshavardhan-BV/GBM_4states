09aa694dbcb764ba21c3afe778657543e3ddd7ee
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2023-09-11 17:39:34 +0530

    Split GSEA correlation and more 2D combinations

--- 4states/2D_GSEA_plot.py
+++ 4states/2D_GSEA_plot.py
@@ -3,6 +3,7 @@ import glob
 import argparse
 import pandas as pd
 import seaborn as sns
+import itertools as it
 import matplotlib.pyplot as plt
 plt.rcParams["svg.hashsalt"]=''
 
@@ -15,35 +16,33 @@ parser.add_argument('-sc', action=argparse.BooleanOptionalAction, help='Add this
 GSE = parser.parse_args().GSE[0]
 sc = parser.parse_args().sc
 
-def aucorr(GSE, GSM):
+def aucorr(GSE, GSM, suff):
     print(GSM)
-    df = pd.read_csv('Output/'+GSE+'/'+suff+'/'+GSM+'-'+suff+'.csv', index_col=0)
+    df = pd.read_csv('Output/'+GSE+'/'+score+'/'+GSM+'-'+score+'.csv', index_col=0)
+    # Select columns with Nef in name
+    df = df.filter(regex=suff)
+    # Take combinations of columns
+    combs = list(it.combinations(df.columns,2))
     # Assign cell type based on which score is max
-    df['CellType'] = df[['NefNPC','NefOPC','NefAC','NefMES']].idxmax(axis=1)
+    df['CellType'] = df.idxmax(axis=1)
     # Create colour paletter
     palette = {'NefNPC':'tab:blue', 'NefOPC':'tab:green', 'NefAC':'tab:orange', 'NefMES':'tab:red', 'VerPN':'tab:blue', 'VerNL':'tab:green', 'VerCL':'tab:orange', 'VerMES':'tab:red'}
-    # Plot the scatter plot
-    sns.scatterplot(data=df, x='NefNPC', y='NefMES', hue='CellType', edgecolor='none', s=10, palette=palette )
-    plt.tight_layout()
-    plt.savefig('figures/'+GSE+'/GSEA/'+GSM+'-2D-Nef.svg')
-    plt.close()
-    plt.clf()
-    # Assign cell type based on which score is max
-    df['CellType'] = df[['VerPN','VerNL', 'VerCL', 'VerMES']].idxmax(axis=1)
-    # Plot the scatter plot
-    sns.scatterplot(data=df, x='VerPN', y='VerMES', hue='CellType',edgecolor='none', s=10, palette=palette)
-    plt.tight_layout()
-    plt.savefig('figures/'+GSE+'/GSEA/'+GSM+'-2D-Ver.svg')
-    plt.close()
-    plt.clf()
+    for combi in combs:
+        # Plot the scatter plot
+        sns.scatterplot(data=df, x=combi[0], y=combi[1], hue='CellType', edgecolor='none', s=10, palette=palette )
+        plt.tight_layout()
+        plt.savefig('figures/'+GSE+'/GSEA/'+GSM+'-2D-{0}vs{1}.svg'.format(*combi))
+        plt.close()
+        plt.clf()
 
 # List of GSM IDs
-suff = 'AUCell' if sc else 'ssGSEA'
-files = glob.glob('Output/'+GSE+'/'+suff+'/*-'+suff+'.csv')
-GSMs = [os.path.basename(x).replace(f'-'+suff+'.csv','') for x in files]
+score = 'AUCell' if sc else 'ssGSEA'
+files = glob.glob('Output/'+GSE+'/'+score+'/*-'+score+'.csv')
+GSMs = [os.path.basename(x).replace(f'-'+score+'.csv','') for x in files]
 # Make directory for output
 os.makedirs('Output/'+GSE+'/GSEA/', exist_ok=True)
 
 # Iterate over GSM samples
 for GSM in GSMs:
-    aucorr(GSE,GSM)
+    aucorr(GSE,GSM,'Nef')
+    aucorr(GSE,GSM,'Ver')
